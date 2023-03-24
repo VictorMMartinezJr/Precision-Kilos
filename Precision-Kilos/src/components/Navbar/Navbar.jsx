2db12d88175932304e8b1f97dc7b52bb3d237670
Victor Martinez Jr <victormtzcodes@gmail.com>
2023-03-24 17:37:27 -0400

    added navbar component on small device

---
+++ Precision-Kilos/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <nav id="navbar">
+      <h2 className="navbar__h2">Precision Kilos</h2>
+    </nav>
+  );
+};
+
+export default Navbar;
