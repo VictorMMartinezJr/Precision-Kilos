57fa0d33d7e6d4247ab13fa6c315fdc615034cb3
Victor Martinez Jr <victormtzcodes@gmail.com>
2023-04-06 20:23:08 -0400

    added useEffect to check if barbell is full after plate gets added

--- Precision-Kilos/src/components/Barbell/Barbell.jsx
+++ Precision-Kilos/src/components/Barbell/Barbell.jsx
@@ -1,12 +1,47 @@
 import "./Barbell.css";
 import barbellSvg from "../../../public/barbell.svg";
 import ClearBtn from "../ClearBtn/ClearBtn";
+import { useRef, useEffect, useContext } from "react";
+import { WeightsContext } from "../WeightsContext";
 
 const Barbell = () => {
+  const { platesOnBar, setSpaceOnBar } = useContext(WeightsContext);
+  const kilosContainer = useRef(null);
+  const kilo = useRef(null);
+  useEffect(() => {
+    const barbellWidth = kilo.current.getBoundingClientRect().width;
+    const barbellContainerWidth =
+      kilosContainer.current.getBoundingClientRect().width - 8;
+
+    if (barbellWidth >= barbellContainerWidth) {
+      setSpaceOnBar(false);
+      return;
+    }
+  }, [platesOnBar]);
+
   return (
     <div className="barbell__container">
       <ClearBtn />
-      <img src={barbellSvg} alt="barbell" className="barbellsvg" />
+      <div className="barbell__img__container">
+        <img src={barbellSvg} alt="barbell" className="barbellsvg" />
+        <div className="kilos__container" ref={kilosContainer}>
+          <ul className="kilos" ref={kilo}>
+            {platesOnBar.map((p, i) => {
+              return (
+                <li
+                  className="kilo"
+                  style={{
+                    height: p.height,
+                    width: p.width,
+                    backgroundColor: p.color,
+                  }}
+                  key={i}
+                ></li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
