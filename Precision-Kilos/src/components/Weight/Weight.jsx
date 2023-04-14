e351839de479e9d4b94efb1453ef1d232af43a02
Victor Martinez Jr <victormtzcodes@gmail.com>
2023-04-13 21:47:18 -0400

    removed decimal from weight

--- Precision-Kilos/src/components/Weight/Weight.jsx
+++ Precision-Kilos/src/components/Weight/Weight.jsx
@@ -6,9 +6,9 @@ const Weight = () => {
   const { kiloWeight, lbWeight, spaceOnBar } = useContext(WeightsContext);
   return (
     <div className="weight__container">
-      <h2 className="weight__number">{kiloWeight.toFixed(1)} KG</h2>
+      <h2 className="weight__number">{kiloWeight} KG</h2>
       <p className="weight__dividericon">|</p>
-      <h2 className="weight__number">{lbWeight.toFixed(1)} LB</h2>
+      <h2 className="weight__number">{lbWeight} LB</h2>
       <p className={`weight__maxed ${spaceOnBar && "hidden"}`}>
         Bar is maxed out!
       </p>
